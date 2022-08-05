8376a1aa51e2bb2c7278b76a33b245098a9d770e
MagomedIbr <58404515+MagomedIbr@users.noreply.github.com>
2022-08-05 20:24:21 +0200

    Delete emg

--- emg/002/001/EMG_BioSpy_Import.py
+++
@@ -1,9 +0,0 @@
-from biosppy import storage
-import scipy
-from biosppy.signals import emg
-
-# load raw ECG signal
-signal, mdata = storage.load_txt('e07_002_001_0348.bdf')
-
-# process it and plot
-out = emg.emg(signal=signal, sampling_rate=1000., show=True)
